08ea72c8a7dd9813980b7d9ec2b32b0f76da9417
NandaMagalhaes <nandamagalhaes26@gmail.com>
2022-03-21 07:42:16 -0300

    script

---
+++ script/script.js
@@ -0,0 +1,10 @@
+//Dando boas vindas para os usuários //
+
+alert("OLÁ VISITANTE!");
+
+let nomevisitante = prompt("Digite seu nome por gentileza.");
+alert("Seja muito bem vindo", +" " + nomevisitante);
+
+alert("No nosso projeto você poderá incluir suas experiências com passeios, cidades turísticas.");
+
+alert("Disponibilizamos para os usuários a inclusão de suas experiências e cidades visitadas.APROVEITE A EXPERIÊNCIA!!!! ");
---
+++ script/scriptform.js
@@ -0,0 +1,3 @@
+let nomevisitante = prompt("Digite seu nome por gentileza.");
+alert("Seja muito bem vindo", +" " + nomevisitante);
+alert("Seja muito bem vindo", +nomevisitante);
