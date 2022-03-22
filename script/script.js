65c317f1efd523d02f5dd392a8e039723b5a1343
NandaMagalhaes <nandamagalhaes26@gmail.com>
2022-03-21 23:39:31 -0300

    novo codigo

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ alert("OLÁ VISITANTE!");
 /*Tentando utilizar Template String */
 
 let nomevisitante = prompt('Digite seu nome por gentileza.');
-let meuTemplate = `Seja muito bem vindo {$nomevisitante}`;
+let meuTemplate = 'Seja muito bem vindo ' + ' ' + nomevisitante;
 alert(meuTemplate);
 
 alert("No nosso projeto você poderá incluir suas experiências com passeios, cidades turísticas.");
@@ -24,3 +24,5 @@ function redMode() {
     body.classList.toggle('redModo');
     card.forEach(div => div.classList.toggle('alteraItem'));
 };
+''
+'' +
--- script/scriptnovos.js
+++ script/scriptnovos.js
@@ -4,18 +4,72 @@ alert("Seja muito bem vindo", +nomevisitante); /*
 
 
 
-/* Criar novos cards a partir da inclusao feita pelo usuário*/
 
-
-/* Capturar quantas cidades (cards o usuário deseja incluir)*/
-
-let quantCards = prompt("Prezado visitante favor incluir quantos Cards(cidades) deseja incluir ?");
 
 /*Capturar informações sobre o card novo que o usuário deseja incluir*/
 
-let nomeCidade = document.querySelector('form.#cidade');
-let textoDescricao = document.querySelector('form.#descricao');
-let nomeUrl = document.querySelector('form.#url');
+let nomeCidade = document.getElementById("cidade");
+let textoDescricao = document.getElementById("descricao");
+let nomeUrl = document.getElementById("url");
+/* Validando o button submit */
+
+let formulario = document.getElementById("botao");
+botaoSubmit.addEventListener(`click`, function(event) {
+    event.preventDefault()
+});
+
+/*CAPTURANDO DADOS DOS CARDS JÁ EXISTENTES*/
+/*criando uma lista com o elemento pai ;*/
+
+
+/*CRIANDO UM NOVO CARD verificar a chamda dos elementos se será com div ou cada elemento*/
+let novoCard = document.createElement('div');
+let nomeCidade = document.createElement('div');
+let textoDescricao = document.createElement('div');
+let infoUrl = document.createElement('div');
+let infoimagem = document.createElement('div');
+
+
+let nomeCidade = document.getElementById('h3');
+let textoDescricao = document.getElementById('span');
+let infoimagem = document.getElementById('img');
+let nomeUrl = document.getElementById('button a');
+
+/* Criar novos cards a partir da inclusao feita pelo usuário*/
+
+novoCard.innerHTML = `
+    <div class = "input-field" >
+        <input type = "text"
+        name = "username"
+        id = "cidade"
+        placeholder = "Registre o nome da cidade" > 
+        <div class = "underline">
+    </div> 
+    <div class = "input-field" >
+        <input type = "text"
+        name = "descricao"
+        id = "descricao"
+        placeholder = "Coloque a descrição da cidade" >
+    <div class = "underline" >
+    </div>
+    <div class = "input-url">
+    < input type = "url"
+        name = "url"
+        id = "url"
+        placeholder = "Url da imagem" > 
+    </div >
+        `
+
+function incluirCidades() {
+    document.cards.appendChild(novoCard);
+}
+
+/*capturando o card onde consta o formulario preenchido pelo usuário*/
+let listaCards = document.querySelector('.cards');
+
+/* Capturar quantas cidades (cards o usuário deseja incluir)*/
+
+let quantCards = prompt("Prezado visitante favor incluir quantos Cards(cidades) deseja incluir ?"); * /
 
 
 /* class =cards  texto h3 recebe o nome da cidade do card de formulário.
@@ -23,59 +77,9 @@ class =cards  texto span recebe o nome da descricao do card de formulário.
 class =cards  img (precisa alterar para URL) recebe o nome da URL do card de formulário.
  */
 /*Temos que dar um jeito de colocar estas informaçoes que o usuario vai digitar dentro dos cards criados*/
-
 for (let i = 0; i < quantCards.length; i++) {
-    h3 = nomeCidade[i].innerText;
-    span = textodescricao[i].innerText;
-    a = nomeUrl[i].innerText;
-}
-
-
-/* Validando o button submit */
-
-let formulario = document.querySelector("form");
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault()
-});
-
-
-
-
-
-
-/*CAPTUTANDO INFORMAÇÕES DO CARD QUE O USUÁRIO INCLUIU DADOS*/
-
-let infousuario = document.querySelectorAll('.input-field');
-
-/*CAPTURANDO DADOS DOS CARDS JÁ EXISTENTES*/
-/*criando uma lista com o elemento pai ;*/
-let listaCards = document.querySelectorAll('.cards');
-
-/*capturando o card onde consta o formulario preenchido pelo usuário*/
-let container = document.querySelector('.container');
-
-/*CRIANDO UM NOVO CARD */
-let novoCard = document.createElement('div');
-let nomeCidade = ;
-let textoDescricao = ;
-let infoUrl = ;
-
-novoCard.innerHTML = `
- <div class="input-field">
-     <input type="text" name="username" id="cidade" placeholder="Registre o nome da cidade">${nomeCidade}
-        <div class="underline"></div>
-        </div>
-            <div class="input-field">
-                <input type="text" name="descricao" id="descricao" placeholder="Coloque a descrição da cidade">${textoDescricao}
-                <div class="underline"></div>
-            </div>
-            <div class="input-url">
-                <input type="url" name="url" id="url" placeholder="Url da imagem">${infoUrl}
-                <label for="nome"></label>
-            </div>
-`
-
-function incluirCidades() {
-    document.container.appendChild(novoCard);
-
+    infoimagem[i].innerText = nomeimagem;
+    nomeCidade[i].innerText = nomecity;
+    textodescricao[i].innerText = descricao;
+    nomeUrl[i].innerText = nomeurl;
 }
